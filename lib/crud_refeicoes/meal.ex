3d4ea0062d22672f161dbef9e75875286918aa87
Wagner Carvalho <wagncarv@gmail.com>
2021-03-28 20:25:33 -0300

    changeset meal

---
+++ lib/crud_refeicoes/meal.ex
@@ -0,0 +1,23 @@
+defmodule Rockelivery.Meal do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @required_params [:descricao, :data, :calorias]
+
+  @derive {Jason.Encoder, only: [:id, :descricao, :data, :calorias]}
+
+  schema "meal" do
+    field :descricao, :string
+    field :data, :utc_datetime
+    field :calorias, :integer
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+  end
+end
